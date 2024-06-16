83cd33724a0d51d134d786b892c5b48cc5b76309
Lexxar91 <myratov91@yandex.ru>
2024-06-16 20:40:13 +0300

    added export

---
+++ app/crud/patents_export.py
@@ -0,0 +1,122 @@
+from io import BytesIO
+from typing import Optional
+
+import pandas as pd
+
+from sqlalchemy import select, func, case
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from fastapi.responses import StreamingResponse
+
+from app.models import Patent, Person, Ownership
+from app.models.filter import FilterTaxNumber
+
+
+async def get_export_patent_file(
+        session: AsyncSession,
+        filter_id: Optional[int] = None,
+        actual: Optional[bool | int] = None,
+        kind: Optional[int] = None,
+
+):
+    """
+      Получает данные о патентах и экспортирует их в файл XLSX.
+
+      Args:
+          session (AsyncSession): Сессия для взаимодействия с базой данных.
+
+          filter_id (Optional[int], optional): Идентификатор фильтра. Если указан, будут экспортированы только
+              патенты, связанные с этим фильтром. Если не указан, будут экспортированы первые 10000 патентов.
+
+          actual (Optional[bool | int], optional): Фильтр по актуальности патента. Если True (1), будут экспортированы
+              только актуальные патенты. Если False (0), будут экспортированы только неактуальные патенты.
+              Если не указан, актуальность не будет учитываться.
+
+          kind (Optional[int], optional): Фильтр по виду патента. Если указан, будут экспортированы только патенты
+              указанного вида (1 - изобретение, 2 - полезная модель, 3 - промышленный образец).
+
+      Returns:
+          StreamingResponse: Поток данных с XLSX-файлом, содержащим информацию о патентах.
+      """
+    stmt = (
+        select(
+            Patent.reg_number,
+            case(
+                (Patent.kind == 1, 'изобретение'),
+                (Patent.kind == 2, 'полезная модель'),
+                (Patent.kind == 3, 'промышленный образец'),
+                else_='неизвестно'
+            ).label('patent_kind'),
+            Patent.reg_date,
+            Patent.name,
+            Patent.actual,
+            Patent.category,
+            Patent.subcategory,
+            Patent.region,
+            Patent.city,
+            func.coalesce(func.array_length(func.string_to_array(Patent.author_raw, ','), 1), 0).label('author_count'),
+            Person.tax_number,
+            case(
+                (Person.kind == 1, 'Юрлицо'),
+                (Person.kind == 2, 'Физлицо/ИП'),
+                else_='неизвестно'
+            ).label('person_kind'),
+            Person.category,
+            Person.full_name,
+        )
+        .join(Ownership, (Ownership.patent_kind == Patent.kind) & (Ownership.patent_reg_number == Patent.reg_number))
+        .join(Person, Person.tax_number == Ownership.person_tax_number)
+
+    )
+
+    if filter_id is None:
+        stmt = stmt.limit(10000)
+
+    if filter_id:
+        stmt = stmt.join(FilterTaxNumber, Person.tax_number == FilterTaxNumber.tax_number)
+        stmt = stmt.where(FilterTaxNumber.filter_id == filter_id)
+
+    if actual:
+        stmt = stmt.where(Patent.actual == actual)
+
+    if kind:
+        stmt = stmt.where(Patent.kind == kind)
+
+    result = await session.execute(stmt)
+    patents = result.all()
+
+    data = [
+        {
+            "ИНН": patent.tax_number,
+            "Вид": patent.person_kind,
+            "Категория": patent.category,
+            "Полное наименование": patent.full_name,
+            "Регистрационный номер патента": patent.reg_number,
+            "Вид патента": patent.patent_kind,
+            "Дата регистрации": patent.reg_date,
+            "Название": patent.name,
+            "Актуальность патента": patent.actual,
+            "Индекс класса по МПК": patent.category,
+            "Индекс подкласса по МПК": patent.subcategory,
+            "Регион правообладателя": patent.region,
+            "Город правообладателя": patent.city,
+            "Число авторов": patent.author_count,
+        }
+        for patent in patents
+    ]
+
+    df = pd.DataFrame(data)
+
+    df["Дата регистрации"] = pd.to_datetime(df["Дата регистрации"]).dt.strftime('%d.%m.%Y')
+
+    buffer = BytesIO()
+    with pd.ExcelWriter(buffer, engine='openpyxl') as writer:
+        df.to_excel(writer, index=False, sheet_name='Patents')
+
+    buffer.seek(0)
+
+    return StreamingResponse(
+        buffer,
+        media_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+        headers={"Content-Disposition": "attachment; filename=patents_export.xlsx"}
+    )
--- app/models/filter.py
+++ app/models/filter.py
@@ -7,8 +7,22 @@ from app.core.db import Base
 
 
 class Filter(Base):
+    """
+    Модель фильтра.
+
+    Атрибуты:
+        id (int): Уникальный идентификатор фильтра.
+        name (str): Название фильтра (уникальное, не более 1000 символов).
+        filename (str): Название файла, из которого был создан фильтр (не более 1000 символов).
+        created (datetime): Дата и время создания фильтра.
+        tax_numbers_count (int): Количество налоговых номеров в фильтре.
+        tax_numbers (list[FilterTaxNumber]): Связанные налоговые номера фильтра.
+
+    Связи:
+        tax_numbers (relationship): Связь "один-ко-многим" с моделью FilterTaxNumber.
+    """
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String(1000), nullable=False)
+    name = Column(String(1000), nullable=False, unique=True)
     filename = Column(String(1000), nullable=False)
     created = Column(DateTime, default=datetime.utcnow)
     tax_numbers_count = Column(Integer, nullable=False)
@@ -17,6 +31,18 @@ class Filter(Base):
 
 
 class FilterTaxNumber(Base):
+    """
+    Модель налогового номера в фильтре.
+
+    Атрибуты:
+        id (int): Уникальный идентификатор налогового номера.
+        filter_id (int): Идентификатор фильтра, к которому относится налоговый номер.
+        tax_number (str): Налоговый номер.
+        filter (Filter): Связанный фильтр.
+
+    Связи:
+        filter (relationship): Связь "многие-к-одному" с моделью Filter.
+    """
     id = Column(Integer, primary_key=True, index=True)
     filter_id = Column(Integer, ForeignKey('filter.id'), nullable=False)
     tax_number = Column(Text, nullable=False)
