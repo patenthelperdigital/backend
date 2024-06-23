0af515cbe47adddad862dc6e437145f787d6666f
Pavel Syomin <pas.ntg@gmail.com>
2024-06-23 21:40:28 +0500

    Restore database_cli_url

--- app/core/config.py
+++ app/core/config.py
@@ -7,6 +7,7 @@ load_dotenv()
 class Settings(BaseSettings):
     app_title: str = 'Сервис анализа патентной активности компаний.'
     database_url: str
+    database_cli_url: str
 
 
     class Config:
