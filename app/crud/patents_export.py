63ea607b2c87dfa708bd159e82d12d687b4c286b
Pavel Syomin <pas.ntg@gmail.com>
2024-06-24 12:44:17 +0500

    Hotfix

--- app/crud/patents_export.py
+++ app/crud/patents_export.py
@@ -59,7 +59,7 @@ async def get_export_patent_file(
                 (Patent.kind == 3, literal_column("'промышленный образец'")),
                 else_=literal_column("'неизвестно'")
             ).label('patent_kind'),
-            Patent.reg_date,
+            text("to_char(Patent.reg_date, 'DD.MM.YYYY')").label('reg_date_formatted'),
             Patent.name,
             Patent.actual,
             Patent.category,
@@ -127,7 +127,7 @@ async def get_export_patent_file(
                 row.full_name,
                 row.reg_number,
                 row.patent_kind,
-                row.reg_date.strftime('%d.%m.%Y'),
+                row.reg_date_formatted,
                 row.name,
                 row.actual,
                 row.category,
